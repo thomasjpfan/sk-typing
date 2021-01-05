afd037f210303adb1b554c253e35adfa967eddb4
Thomas J. Fan <thomasjpfan@gmail.com>
2021-01-04 21:13:05 -0500

    ENH Adds CVSplitter protocol

--- sk_typing/typing.py
+++ sk_typing/typing.py
@@ -1,3 +1,17 @@
-from typing import Any
+import numpy as np
 
-CV = Any
+from typing import Union
+from typing import Protocol
+from typing import Iterator
+from typing import Literal
+
+
+class CVSplitter(Protocol):
+    def get_n_splits(self):
+        ...
+
+    def split(self, X, y=None, groups=None):
+        ...
+
+
+CV = Union[int, CVSplitter, Iterator[np.array, np.array], Literal["prefit"], None]
